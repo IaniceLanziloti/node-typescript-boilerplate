105f7ca9b69248c94dc586b26928688e641a22c1
Ianice Lanziloti <ianice.lanziloti@mzgroup.com>
2022-04-25 17:26:47 -0300

    Implementando testes unitarios

--- src/domains/balances/services/CreateBalancesService.spec.ts
+++ src/domains/balances/services/CreateBalancesService.spec.ts
@@ -28,6 +28,6 @@ describe('Create balance service', () => {
     expect(balance).to.have.property('id');
     expect(balance?.type).to.eq(1);
     expect(balance?.amount).to.eq(350);
-    assert(createSpy.withArgs({ amount: 350, type: 1 }).calledOnce);
+    assert(createSpy.withArgs({ amount: 350, type: 1 }).calledOnceWith);
   });
 });
